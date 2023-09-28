aa9750557080988c9124078a7d169a7cc9113afc
hanh42 <91781354+hanh42@users.noreply.github.com>
2023-09-28 22:50:30 +0700

    2023/9/28 10:49 PM finish business post screen

---
+++ src/constants/BusinessPost.tsx
@@ -0,0 +1,6 @@
+export const NUM_OF_LINES = 5
+export const TEXT_AVAILABLE = 'Khả dụng'
+export const TEXT_SEE_MORE = 'xem thêm'
+export const TEXT_HIDE_LESS = 'ẩn bớt'
+export const HEADER_ICON_SIZE = 15
+export const BOTTOM_ICON_SIZE = 30
---
+++ src/types/BusinessPost.ts
@@ -0,0 +1,14 @@
+export interface BusinessPost{
+    name:string,
+    avatar:string,
+    typeAuthor:string,
+    available:boolean,
+    timeCreatePost:string,
+    content:string,
+    images:[] | null,
+    type:string,
+    likes:number,
+    isLike:boolean,
+    comments:number,
+    isComment:boolean
+}
