ba93bd5f3b99849f77ce90463a6095b3aec308ff
LamLaiGiaBao <lamlaigiabao2003@gmail.com>
2023-10-03 12:15:21 +0700

    update

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -5,7 +5,82 @@ import { useEffect, useState } from "react";
 import { Dropdown } from 'react-native-element-dropdown'
 import Icon from 'react-native-vector-icons/FontAwesome5';
 
+const dataNew = [
+  {
+    "id": 1,
+    "name": "Ban da dang ky thanh cong!!!",
+    "time": "1 phut truoc",
+    "image": "https://file1.dangcongsan.vn/DATA/0/2018/10/68___gi%E1%BA%BFng_l%C3%A0ng_qu%E1%BA%A3ng_ph%C3%BA_c%E1%BA%A7u__%E1%BB%A9ng_h%C3%B2a___%E1%BA%A3nh_vi%E1%BA%BFt_m%E1%BA%A1nh-16_51_07_908.jpg"
+  },
+  {
+    "id": 2,
+    "name": "John da dang ky thanh cong!!",
+    "time": "2 gio truoc",
+    "image": "https://toanthaydinh.com/wp-content/uploads/2020/04/anh-dep-hoa-huong-duong-va-mat-troi_022805970-1-1181x800-6.jpg"
+  },
+  {
+    "id": 3,
+    "name": "Cong ty .......... vua dang thong tin tuyen dung ",
+    "time": "3 ngay truoc",
+    "image": "https://kenh14cdn.com/thumb_w/660/2020/7/17/brvn-15950048783381206275371.jpg"
+  },
+  {
+    "id": 4,
+    "name": "Ban da dang ky thanh cong!!!",
+    "time": "4 phut truoc",
+    "image": "https://file1.dangcongsan.vn/DATA/0/2018/10/68___gi%E1%BA%BFng_l%C3%A0ng_qu%E1%BA%A3ng_ph%C3%BA_c%E1%BA%A7u__%E1%BB%A9ng_h%C3%B2a___%E1%BA%A3nh_vi%E1%BA%BFt_m%E1%BA%A1nh-16_51_07_908.jpg"
+  },
+  {
+    "id": 5,
+    "name": "John da dang ky thanh cong!!",
+    "time": "5 gio truoc",
+    "image": "https://toanthaydinh.com/wp-content/uploads/2020/04/anh-dep-hoa-huong-duong-va-mat-troi_022805970-1-1181x800-6.jpg"
+  },
+  {
+    "id": 6,
+    "name": "Cong ty .......... vua dang thong tin tuyen dung ",
+    "time": "6 ngay truoc",
+    "image": "https://kenh14cdn.com/thumb_w/660/2020/7/17/brvn-15950048783381206275371.jpg"
+  },
+  {
+    "id": 7,
+    "name": "Ban da dang ky thanh cong!!!",
+    "time": "7 phut truoc",
+    "image": "https://file1.dangcongsan.vn/DATA/0/2018/10/68___gi%E1%BA%BFng_l%C3%A0ng_qu%E1%BA%A3ng_ph%C3%BA_c%E1%BA%A7u__%E1%BB%A9ng_h%C3%B2a___%E1%BA%A3nh_vi%E1%BA%BFt_m%E1%BA%A1nh-16_51_07_908.jpg"
+  },
+  {
+    "id": 8,
+    "name": "John da dang ky thanh cong!!",
+    "time": "8 gio truoc",
+    "image": "https://toanthaydinh.com/wp-content/uploads/2020/04/anh-dep-hoa-huong-duong-va-mat-troi_022805970-1-1181x800-6.jpg"
+  },
 
+  {
+    "id": 9,
+    "name": "John da dang ky thanh cong!!",
+    "time": "9 gio truoc",
+    "image": "https://toanthaydinh.com/wp-content/uploads/2020/04/anh-dep-hoa-huong-duong-va-mat-troi_022805970-1-1181x800-6.jpg"
+  },
+  {
+    "id": 10,
+    "name": "John da dang ky thanh cong!!",
+    "time": "10 gio truoc",
+    "image": "https://toanthaydinh.com/wp-content/uploads/2020/04/anh-dep-hoa-huong-duong-va-mat-troi_022805970-1-1181x800-6.jpg"
+  },
+  {
+    "id": 11,
+    "name": "John da dang ky thanh cong!!",
+    "time": "11 gio truoc",
+    "image": "https://toanthaydinh.com/wp-content/uploads/2020/04/anh-dep-hoa-huong-duong-va-mat-troi_022805970-1-1181x800-6.jpg"
+  },
+  {
+    "id": 12,
+    "name": "John da dang ky thanh cong!!",
+    "time": "12 gio truoc",
+    "image": "https://toanthaydinh.com/wp-content/uploads/2020/04/anh-dep-hoa-huong-duong-va-mat-troi_022805970-1-1181x800-6.jpg"
+  },
+
+]
 
 const { height, width } = Dimensions.get('screen')
 // man hinh tim kiem
@@ -24,15 +99,15 @@ export default function SearchScreen() {
 
 
 
-  useEffect(() => {
-    // document.title = title
-    fetch(`https://jsonplaceholder.typicode.com/${type}`)
-      .then(res => res.json())
-      .then(posts => {
-        setMasterData(posts)
-        setFilterData(posts)
-      })
-  }, [type])
+  // useEffect(() => {
+  //   // document.title = title
+  //   fetch(`https://jsonplaceholder.typicode.com/${type}`)
+  //     .then(res => res.json())
+  //     .then(posts => {
+  //       setMasterData(posts)
+  //       setFilterData(posts)
+  //     })
+  // }, [type])
 
   const postItems = (item: any, index: any) => { }
 
@@ -47,9 +122,7 @@ export default function SearchScreen() {
           <Image
             style={styles.image}
             source={{ uri: "https://file1.dangcongsan.vn/DATA/0/2018/10/68___gi%E1%BA%BFng_l%C3%A0ng_qu%E1%BA%A3ng_ph%C3%BA_c%E1%BA%A7u__%E1%BB%A9ng_h%C3%B2a___%E1%BA%A3nh_vi%E1%BA%BFt_m%E1%BA%A1nh-16_51_07_908.jpg" }} />
-          <Text style={styles.name}
-          >{index + ". " + item.title}</Text>
-
+          <Text style={styles.name}>{index + ". " + item.name}</Text>
         </View>
         <View>
           {
@@ -132,7 +205,7 @@ export default function SearchScreen() {
       <ScrollView >
         <Text style={styles.qty}>Kết quả tìm kiếm ({qty})</Text>
         {
-          filterData.map((item, index) => renderItem(item, index))
+          dataNew.map((item, index) => renderItem(item, index))
         }
       </ScrollView>
     </View>
