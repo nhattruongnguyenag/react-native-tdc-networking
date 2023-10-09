f0d161cec5f9cc49764aa97f074a8ffb78532829
LamLaiGiaBao <lamlaigiabao2003@gmail.com>
2023-10-09 11:12:21 +0700

    commit notification screen

--- src/screens/NotificationScreen.tsx
+++ src/screens/NotificationScreen.tsx
@@ -1,90 +1,139 @@
-import { Dimensions, StyleSheet, Text, View, Image, SafeAreaView } from 'react-native'
-import React from 'react'
-import { FlatList, TouchableOpacity } from 'react-native-gesture-handler'
+import { Dimensions, StyleSheet, Text, View, Image, SafeAreaView, ScrollView, Pressable, Vibration, Alert } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { FlatList, TextInput, TouchableOpacity } from 'react-native-gesture-handler'
 import Icon from 'react-native-vector-icons/FontAwesome5';
+import Icon1 from 'react-native-vector-icons/Entypo';
+import { SERVER_ADDRESS } from '../constants/SystemConstant';
+import axios from 'axios'
+import { Menu, MenuOptions, MenuOption, MenuTrigger, } from 'react-native-popup-menu';
+import { Item } from 'react-native-paper/lib/typescript/components/Drawer/Drawer';
+import { ParamListBase, useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { SEACRH_SCREEN } from '../constants/Screen';
 
 
 const { height, width } = Dimensions.get('screen')
 
-const dataNew = [
-  {
-    "id": 1,
-    "name": "Ban da dang ky thanh cong!!!",
-    "time": "1 phut truoc",
-    "image": "https://file1.dangcongsan.vn/DATA/0/2018/10/68___gi%E1%BA%BFng_l%C3%A0ng_qu%E1%BA%A3ng_ph%C3%BA_c%E1%BA%A7u__%E1%BB%A9ng_h%C3%B2a___%E1%BA%A3nh_vi%E1%BA%BFt_m%E1%BA%A1nh-16_51_07_908.jpg"
-  },
-  {
-    "id": 2,
-    "name": "John da dang ky thanh cong!!",
-    "time": "2 gio truoc",
-    "image": "https://toanthaydinh.com/wp-content/uploads/2020/04/anh-dep-hoa-huong-duong-va-mat-troi_022805970-1-1181x800-6.jpg"
-  },
-  {
-    "id": 3,
-    "name": "Cong ty .......... vua dang thong tin tuyen dung ",
-    "time": "3 ngay truoc",
-    "image": "https://kenh14cdn.com/thumb_w/660/2020/7/17/brvn-15950048783381206275371.jpg"
-  },
-  {
-    "id": 4,
-    "name": "Ban da dang ky thanh cong!!!",
-    "time": "4 phut truoc",
-    "image": "https://file1.dangcongsan.vn/DATA/0/2018/10/68___gi%E1%BA%BFng_l%C3%A0ng_qu%E1%BA%A3ng_ph%C3%BA_c%E1%BA%A7u__%E1%BB%A9ng_h%C3%B2a___%E1%BA%A3nh_vi%E1%BA%BFt_m%E1%BA%A1nh-16_51_07_908.jpg"
-  },
-  {
-    "id": 5,
-    "name": "John da dang ky thanh cong!!",
-    "time": "5 gio truoc",
-    "image": "https://toanthaydinh.com/wp-content/uploads/2020/04/anh-dep-hoa-huong-duong-va-mat-troi_022805970-1-1181x800-6.jpg"
-  },
-  {
-    "id": 6,
-    "name": "Cong ty .......... vua dang thong tin tuyen dung ",
-    "time": "6 ngay truoc",
-    "image": "https://kenh14cdn.com/thumb_w/660/2020/7/17/brvn-15950048783381206275371.jpg"
-  },
-  {
-    "id": 7,
-    "name": "Ban da dang ky thanh cong!!!",
-    "time": "7 phut truoc",
-    "image": "https://file1.dangcongsan.vn/DATA/0/2018/10/68___gi%E1%BA%BFng_l%C3%A0ng_qu%E1%BA%A3ng_ph%C3%BA_c%E1%BA%A7u__%E1%BB%A9ng_h%C3%B2a___%E1%BA%A3nh_vi%E1%BA%BFt_m%E1%BA%A1nh-16_51_07_908.jpg"
-  },
-  {
-    "id": 8,
-    "name": "John da dang ky thanh cong!!",
-    "time": "8 gio truoc",
-    "image": "https://toanthaydinh.com/wp-content/uploads/2020/04/anh-dep-hoa-huong-duong-va-mat-troi_022805970-1-1181x800-6.jpg"
-  },
-
-  {
-    "id": 9,
-    "name": "John da dang ky thanh cong!!",
-    "time": "9 gio truoc",
-    "image": "https://toanthaydinh.com/wp-content/uploads/2020/04/anh-dep-hoa-huong-duong-va-mat-troi_022805970-1-1181x800-6.jpg"
-  },
-
-
-
-]
-
 // man hinh hien thi danh sach thong bao
 export default function NotificationScreen() {
+  const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>()
+  // const [menuRef, setMenuRef] = useState<Menu | null>()
+  const [data, setData] = useState([]);
+  const [dataIsRead, setDataIsRead] = useState([])
+  const [isMenuOpen, setMenuOpen] = useState(false)
+  const [search, setSearch] = useState('')
+  const [openSearch, setOpenSearch] = useState(false)
+
+
+
+  useEffect(() => {
+    axios.get(`${SERVER_ADDRESS}/api/notifications`)
+      .then(res => {
+        const respo = res.data.data;
+        setData(respo)
+      })
+      .catch(error => console.log(error));
+  }, [])
+
+  const handleIsRead = (id: any, userId: any) => {
+    try {
+      axios.put(`${SERVER_ADDRESS}/api/notifications/changeStatus/makeNotSeen`, {
+        id: id,
+        userId: userId
+      })
+        .then((response) => {
+          if (response.status === 204) {
+            console.log('that bai');
+          } else {
+
+            console.log('thanh cong');
+          }
+        })
+    } catch (error) {
+      console.error('Error updating name:', error);
+    }
+  }
+
+  const handleDelNotification = (id: number, userId: number) => {
+
+    try {
+      axios.delete(`${SERVER_ADDRESS}/api/notifications/`, { data: { id: id, userId: userId } })
+    } catch (error) {
+      console.error('Error updating name:', error);
+    }
+  }
+
+  const handleItem = (id: number, userId: number) => {
+    try {
+      // navigation.navigate('Man hinh muon den', { id: id })
+      axios.put(`${SERVER_ADDRESS}/api/notifications/changeStatus`, {
+        id: id,
+        userId: userId
+      })
+    } catch (error) {
+      console.error('Error updating name:', error);
+    }
+  }
+
+  const handleListIsRead = () => {
+    axios.put(`${SERVER_ADDRESS}/api/notifications/changeStatus/all`, { userId: 2 })
+      .catch(error => console.log(error));
+  }
+
+
+  const handleOpenSearch = () => {
+    setOpenSearch(!openSearch);
+  }
+
   //Render Items
   const renderItem = (item: any, index: any) => {
     return (
-      <View
-        key={index}
-        style={styles.item}>
-        <Image
-          style={styles.image}
-          source={{ uri: item.image }} />
-        <Text style={styles.name}
-        >{item.name}</Text>
-        <View style={styles.time}>
-          <Text style={styles.tg}>{item.time}</Text>
-
-        </View>
+      <View>
+        <Pressable
+          onPress={() => handleItem(item.id, item.user.id)}
+          key={index}
+          style={[styles.item, { backgroundColor: item.status === '1' ? '#ffffff' : '#f3f9ff' }]}>
+          <View style={styles.cont}>
+            <Image
+              style={styles.image}
+              source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7h13cetHlwG784hz57YxCRBAfacOVhCmPrt0EoVRAcg&s' }} />
+            <View style={styles.content}>
+              <Text style={styles.name}>{item.content}</Text>
+              <Text style={styles.tg}>15 phut truoc</Text>
+            </View>
+          </View>
+          <Menu style={styles.menu}
+            key={item.id}
+            onOpen={() => setMenuOpen(true)
+            }
+            onClose={() => setMenuOpen(false)
+            }
+          // ref={(ref) => {
+          //   setMenuRef(ref)
+          // }}
+          >
+            <MenuTrigger
+            >
+              <Icon1 name="dots-three-vertical" size={17} color="#000000" />
+            </MenuTrigger>
+            <MenuOptions
+              optionsContainerStyle={{ marginLeft: 50, marginTop: 25, borderRadius: 10 }}>
+              <MenuOption
+                onSelect={() => handleDelNotification(item.id, item.user.id)}
+              >
+                <Text style={styles.option}>Xóa thông báo</Text>
+              </MenuOption>
+              <MenuOption
+                onSelect={() => handleIsRead(item.id, item.user.id)}
+              >
+                <Text style={styles.option}>Đánh dấu chưa đọc</Text>
+              </MenuOption>
+            </MenuOptions>
+          </Menu>
+        </Pressable>
       </View>
+
+
     )
   }
 
@@ -92,34 +141,50 @@ export default function NotificationScreen() {
     <>
       <View style={styles.screen}>
         {/* Select */}
-        <View style={styles.select}>
-        <View style={styles.txtN}>
-          <Text style={styles.txt}>Thông báo</Text>
+        <View style={[styles.operation, {height: openSearch ? height*0.168 : height*0.1}]}>
+          <View style={styles.select}>
+            <View style={styles.txtN}>
+              <Text style={styles.txt}>Thông báo</Text>
+            </View>
+            <View style={styles.tick}>
+              <TouchableOpacity style={styles.tickButton}
+                onPress={handleListIsRead}
+              >
+                <Text style={styles.txtTick}>
+                  Đánh dấu tất cả đã đọc
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.searchBtn}>
+              <TouchableOpacity style={styles.searchButton}
+              onPress={handleOpenSearch}
+              >
+                <Text>
+                  <Icon name="search" size={20} color="#ffffff" />
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+          {openSearch && (<View >
+            <TextInput 
+            value={search}
+            style={styles.search}
+             placeholder='Tìm kiếm thông báo...'/>
+          </View>)}
+          
         </View>
-        <View style={styles.tick}>
-          <TouchableOpacity style={styles.tickButton}>
-            <Text style={styles.txtTick}>
-              Đánh dấu tất cả đã đọc
-            </Text>
-          </TouchableOpacity>
-        </View>
-        <View style={styles.search}>
-          <TouchableOpacity style={styles.searchButton}>
-            <Text>
-              <Icon name="search" size={20} color="#ffffff" />
-            </Text>
-          </TouchableOpacity>
-        </View>
-        </View>
-        {/* Flatlist */}
-        <View style={styles.platList}>
-          <FlatList
-            showsVerticalScrollIndicator={false}
-            data={dataNew}
-            renderItem={({ item, index }) => renderItem(item, index)}
-          />
-        </View>
-      </View>
+        {/*  */}
+        <ScrollView style={styles.platList}>
+          {
+            (data !== null ? (
+              data.map((item, index) => (
+                renderItem(item, index)
+                // <Text>{JSON.stringify(data)}</Text>
+              ))
+            ) : null)
+          }
+        </ScrollView >
+      </View >
     </>
 
   )
@@ -128,12 +193,23 @@ export default function NotificationScreen() {
 const styles = StyleSheet.create({
   screen: {
     backgroundColor: '#ffffff',
-    flex: 10
+    flex: 1
+  },
+  operation: {
+    justifyContent: 'center'
   },
   select: {
-    flex: 1,
     flexDirection: 'row',
-    height: 100,
+  },
+  search: {
+    marginTop: 10,
+    marginLeft: 10,
+    marginRight: 10,
+    paddingLeft: 15,
+    backgroundColor: '#d9d9d9',
+    borderRadius: 5,
+    height: 40,
+    alignItems: 'center'
   },
   //Text thong báo
   txtN: {
@@ -166,7 +242,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   //Search
-  search: {
+  searchBtn: {
     flex: 1,
     justifyContent: 'center',
     marginLeft: 5
@@ -181,39 +257,53 @@ const styles = StyleSheet.create({
   },
   //Flatlist
   platList: {
-    flex: 9,
     width: '100%',
-    backgroundColor: 'blue'
   },
   item: {
     flexDirection: 'row',
+    justifyContent: 'space-between',
     height: 80,
-    backgroundColor: '#f3f9ff',
+    // backgroundColor: '#f3f9ff',
     paddingTop: 4,
+    paddingBottom: 4,
     paddingLeft: 10,
     paddingRight: 15,
     borderBottomWidth: 0.8,
     borderBottomColor: 'grey'
   },
   image: {
-    flex: 1,
+    width: 70,
     height: 70,
     borderRadius: 50,
+    paddingVertical: 20,
+    borderColor: '#0065ff',
+    borderWidth: 1
+  },
+  cont: {
+    flexDirection: 'row'
+  },
+  content: {
+    paddingTop: 8,
+    paddingLeft: 10,
+    width: '80%',
   },
   name: {
-    flex: 3,
-    paddingLeft: 15,
-    paddingTop: 8,
     color: '#000000',
     fontSize: 17,
-  },
-  time: {
-    flex: 1.4,
-    alignItems: 'flex-end',
-    paddingTop: 6
   },
   tg: {
     fontSize: 15,
     color: '#B9B6B6',
+    paddingBottom: 0
+  },
+  menu: {
+    justifyContent: 'center',
+  },
+  option: {
+    fontSize: 15,
+    paddingTop: 7,
+    paddingBottom: 7,
+    paddingLeft: 5,
   }
+
 })
