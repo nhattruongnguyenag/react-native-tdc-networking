831fa98f1e3218fc5d76b716cd71cdd126cb90a7
LamLaiGiaBao <lamlaigiabao2003@gmail.com>
2023-10-11 11:09:00 +0700

    commit search screen

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,88 +1,13 @@
 import { Dimensions, StyleSheet, Text, View, Button, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { FlatList, ScrollView, TextInput } from 'react-native-gesture-handler'
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { Dropdown } from 'react-native-element-dropdown'
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import { SERVER_ADDRESS } from '../constants/SystemConstant';
 import axios from 'axios';
 
-const dataNew = [
-  {
-    "id": 1,
-    "name": "Ban da dang ky thanh cong!!!",
-    "time": "1 phut truoc",
-    "image": "https://file1.dangcongsan.vn/DATA/0/2018/10/68___gi%E1%BA%BFng_l%C3%A0ng_qu%E1%BA%A3ng_ph%C3%BA_c%E1%BA%A7u__%E1%BB%A9ng_h%C3%B2a___%E1%BA%A3nh_vi%E1%BA%BFt_m%E1%BA%A1nh-16_51_07_908.jpg"
-  },
-  {
-    "id": 2,
-    "name": "John da dang ky thanh cong!!",
-    "time": "2 gio truoc",
-    "image": "https://toanthaydinh.com/wp-content/uploads/2020/04/anh-dep-hoa-huong-duong-va-mat-troi_022805970-1-1181x800-6.jpg"
-  },
-  {
-    "id": 3,
-    "name": "Cong ty .......... vua dang thong tin tuyen dung ",
-    "time": "3 ngay truoc",
-    "image": "https://kenh14cdn.com/thumb_w/660/2020/7/17/brvn-15950048783381206275371.jpg"
-  },
-  {
-    "id": 4,
-    "name": "Ban da dang ky thanh cong!!!",
-    "time": "4 phut truoc",
-    "image": "https://file1.dangcongsan.vn/DATA/0/2018/10/68___gi%E1%BA%BFng_l%C3%A0ng_qu%E1%BA%A3ng_ph%C3%BA_c%E1%BA%A7u__%E1%BB%A9ng_h%C3%B2a___%E1%BA%A3nh_vi%E1%BA%BFt_m%E1%BA%A1nh-16_51_07_908.jpg"
-  },
-  {
-    "id": 5,
-    "name": "John da dang ky thanh cong!!",
-    "time": "5 gio truoc",
-    "image": "https://toanthaydinh.com/wp-content/uploads/2020/04/anh-dep-hoa-huong-duong-va-mat-troi_022805970-1-1181x800-6.jpg"
-  },
-  {
-    "id": 6,
-    "name": "Cong ty .......... vua dang thong tin tuyen dung ",
-    "time": "6 ngay truoc",
-    "image": "https://kenh14cdn.com/thumb_w/660/2020/7/17/brvn-15950048783381206275371.jpg"
-  },
-  {
-    "id": 7,
-    "name": "Ban da dang ky thanh cong!!!",
-    "time": "7 phut truoc",
-    "image": "https://file1.dangcongsan.vn/DATA/0/2018/10/68___gi%E1%BA%BFng_l%C3%A0ng_qu%E1%BA%A3ng_ph%C3%BA_c%E1%BA%A7u__%E1%BB%A9ng_h%C3%B2a___%E1%BA%A3nh_vi%E1%BA%BFt_m%E1%BA%A1nh-16_51_07_908.jpg"
-  },
-  {
-    "id": 8,
-    "name": "John da dang ky thanh cong!!",
-    "time": "8 gio truoc",
-    "image": "https://toanthaydinh.com/wp-content/uploads/2020/04/anh-dep-hoa-huong-duong-va-mat-troi_022805970-1-1181x800-6.jpg"
-  },
 
-  {
-    "id": 9,
-    "name": "John da dang ky thanh cong!!",
-    "time": "9 gio truoc",
-    "image": "https://toanthaydinh.com/wp-content/uploads/2020/04/anh-dep-hoa-huong-duong-va-mat-troi_022805970-1-1181x800-6.jpg"
-  },
-  {
-    "id": 10,
-    "name": "John da dang ky thanh cong!!",
-    "time": "10 gio truoc",
-    "image": "https://toanthaydinh.com/wp-content/uploads/2020/04/anh-dep-hoa-huong-duong-va-mat-troi_022805970-1-1181x800-6.jpg"
-  },
-  {
-    "id": 11,
-    "name": "John da dang ky thanh cong!!",
-    "time": "11 gio truoc",
-    "image": "https://toanthaydinh.com/wp-content/uploads/2020/04/anh-dep-hoa-huong-duong-va-mat-troi_022805970-1-1181x800-6.jpg"
-  },
-  {
-    "id": 12,
-    "name": "John da dang ky thanh cong!!",
-    "time": "12 gio truoc",
-    "image": "https://toanthaydinh.com/wp-content/uploads/2020/04/anh-dep-hoa-huong-duong-va-mat-troi_022805970-1-1181x800-6.jpg"
-  },
-
-]
 
 const { height, width } = Dimensions.get('screen')
 // man hinh tim kiem
@@ -94,20 +19,27 @@ export default function SearchScreen() {
   const [qty, setQty] = useState('')
   const [value, setValue] = useState(null);
   const [items, setItems] = useState([
-    { label: 'Người dùng', value: 'users' },
-    { label: 'Doanh nghiệp', value: 'business' },
+    { label: 'Sinh viên', value: 'sinh-vien' },
+    { label: 'Doanh nghiệp', value: 'doanh-nghiep' },
     { label: 'Bài viết', value: 'posts' }
   ]);
-  const URL = `${SERVER_ADDRESS}/api/${type}`
+  
+  const handleSearch = async () => {
+    try {
+      console.log(type + ' - ' + search)
+      await axios.post(`${SERVER_ADDRESS}/api/find/user`, {
+        type: type,
+        name: search
+      }).then(res => {
+        setMasterData(res.data.data);
+        setSearch('')
+      })
+    } catch (error) {
+      console.error('Lỗi trong quá trình tìm kiếm: ', error);
+    }
+  };
 
-  useEffect(() => {
-    console.log(type);
-    axios.get(URL)
-    .then((response) => {
-      const res = response.data.data
-      setMasterData(res)
-    })
-  }, [type])
+
 
   //Render Posts Item
   const postItems = (item: any, index: any) => { }
@@ -158,7 +90,7 @@ export default function SearchScreen() {
           placeholder='Nhập nội dung tìm kiếm...'
           placeholderTextColor='#000000'
           value={search}
-          // onChangeText={(txt) => searchFilter(txt)}
+          onChangeText={(txt) => setSearch(txt)}
         ></TextInput>
         <View style={styles.select}>
           <View style={styles.drop}>
@@ -174,7 +106,9 @@ export default function SearchScreen() {
               }}
             />
           </View>
-          <TouchableOpacity style={styles.btnSearch}>
+          <TouchableOpacity style={styles.btnSearch}
+            onPress={handleSearch}
+          >
             <Text>
               <Icon name="search" size={20} color="#ffffff" />
             </Text>
@@ -196,7 +130,6 @@ const styles = StyleSheet.create({
   searchScreen: {
     backgroundColor: 'white',
     flex: 1
-
   },
   operation: {
     marginLeft: 12,
@@ -209,7 +142,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#d9d9d9',
     borderRadius: 5,
     height: 40,
-    paddingLeft: 10 
+    paddingLeft: 10
   },
   select: {
     flexDirection: 'row',
@@ -293,22 +226,22 @@ const styles = StyleSheet.create({
   },
 
 })
-  //
-  // const searchFilter = (txt: any) => {
-  //   if (txt) {
-  //     const newData = masterData.filter(
-  //       function (item: any, index: any) {
-  //         const itemData = item.title ? item.title.toUpperCase() : ''.toUpperCase()
-  //         const textData = txt.toUpperCase();
-  //         const i = itemData.indexOf(textData) > -1;
-  //         return i;
-  //       }
-  //     )
-  //     setFilterData(newData)
-  //     setSearch(txt)
+//
+// const searchFilter = (txt: any) => {
+//   if (txt) {
+//     const newData = masterData.filter(
+//       function (item: any, index: any) {
+//         const itemData = item.title ? item.title.toUpperCase() : ''.toUpperCase()
+//         const textData = txt.toUpperCase();
+//         const i = itemData.indexOf(textData) > -1;
+//         return i;
+//       }
+//     )
+//     setFilterData(newData)
+//     setSearch(txt)
 
-  //   } else {
-  //     setFilterData(masterData)
-  //     setSearch(txt)
-  //   }
-  // }
+//   } else {
+//     setFilterData(masterData)
+//     setSearch(txt)
+//   }
+// }
