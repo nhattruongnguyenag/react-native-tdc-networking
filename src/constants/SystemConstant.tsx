3de71e1b05e4dcf9e3c1f584596f8f2322a16423
LamLaiGiaBao <lamlaigiabao2003@gmail.com>
2023-10-16 10:33:58 +0700

    commit

--- src/constants/SystemConstant.tsx
+++ src/constants/SystemConstant.tsx
@@ -1,8 +1,4 @@
 import { SPLASH_SCREEN, TOP_TAB_NAVIGATOR, LOGIN_SCREEN, IMAGE_VIEW_SCREEN, CREATE_SURVEY_SCREEN } from './Screen'
 
 export const INITIAL_SCREEN = SPLASH_SCREEN
-<<<<<<< HEAD
 export const SERVER_ADDRESS = "http://192.168.43.64:8080";
-=======
-export const SERVER_ADDRESS = 'http://192.168.1.19:8080/'
->>>>>>> develop
