9c400c89a3f8f63b1dfbfc852dbfd1916902cc67
LamLaiGiaBao <lamlaigiabao2003@gmail.com>
2023-11-01 09:38:08 +0700

    commit list post saved

---
+++ src/screens/ListPostSavedScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ListPostSavedScreen = () => {
+  return (
+    <View>
+      <Text>ListPostSavedScreen</Text>
+    </View>
+  )
+}
+
+export default ListPostSavedScreen
