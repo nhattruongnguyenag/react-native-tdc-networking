c68389783312db9b611aa081a04b7ebc3ebbf537
LamLaiGiaBao <lamlaigiabao2003@gmail.com>
2023-11-06 07:39:42 +0700

    commit post saved

--- src/screens/ListPostSavedScreen.tsx
+++ src/screens/ListPostSavedScreen.tsx
@@ -1,12 +1,31 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
 const ListPostSavedScreen = () => {
   return (
-    <View>
-      <Text>ListPostSavedScreen</Text>
+    <View style={styles.searchScreen}>
+
+      <View style={styles.operation}>
+        <Text>Hello</Text>
+      </View>
+
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  searchScreen: {
+    backgroundColor: 'white',
+    flex: 1
+  },
+  operation: {
+    marginLeft: 12,
+    marginRight: 12,
+    marginTop: 15,
+    marginBottom: 50,
+
+    backgroundColor: 'red'
+  },
+})
 
 export default ListPostSavedScreen
