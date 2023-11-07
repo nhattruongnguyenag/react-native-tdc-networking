d4bfb3029759001404cffdedec1318a30c517625
LamLaiGiaBao <lamlaigiabao2003@gmail.com>
2023-11-07 14:49:09 +0700

    commit detail job apply

--- src/screens/DetailJobApplyScreen.tsx
+++ src/screens/DetailJobApplyScreen.tsx
@@ -1,12 +1,8 @@
 import { Dimensions, StyleSheet, Text, View, Button, Image, TouchableOpacity, Pressable } from 'react-native'
 import React, { useEffect, useState } from 'react'
-import { Dropdown } from 'react-native-element-dropdown'
 import axios from 'axios';
 import { SERVER_ADDRESS } from '../constants/SystemConstant';
-import { Post } from '../types/Post';
-import { ScrollView } from 'react-native-gesture-handler';
 import PDF from 'react-native-pdf';
-import DocumentPicker from 'react-native-document-picker';
 import { FileUploadRequest } from '../types/request/FileUploadRequest';
 
 
@@ -32,7 +28,7 @@ const DetailJobApplyScreen = () => {
   
 
   return (
-    <View style={styles.searchScreen}>
+    <View style={styles.detailJobAppyScreen}>
 
       <PDF
         trustAllCerts={false}
@@ -56,24 +52,9 @@ const DetailJobApplyScreen = () => {
 }
 
 const styles = StyleSheet.create({
-  searchScreen: {
+    detailJobAppyScreen: {
     backgroundColor: 'white',
     flex: 1
-  },
-  operation: {
-    flexDirection: 'row',
-    marginLeft: 12,
-    marginRight: 12,
-    marginTop: 15,
-    marginBottom: 20,
-  },
-  dropDown: {
-    flex: 1,
-    height: 50,
-    borderColor: 'gray',
-    borderWidth: 0.5,
-    borderRadius: 8,
-    paddingHorizontal: 8,
   },
 })
 
